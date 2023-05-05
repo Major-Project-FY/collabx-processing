e09b862734a7b80337fe82e67b1d5baab804522a
Mayuresh-loves-cpp <mvshinde640@gmail.com>
2023-05-05 14:21:30 +0530

    processing: updated config

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # importing routes
 from routes import routes
 
+# module imports
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+port = os.getenv('PORT')
+
 # serving app
 if __name__ == "__main__":
-    routes.app.run(port=4000, debug=True)
+    routes.app.run(port=port, debug=True)
